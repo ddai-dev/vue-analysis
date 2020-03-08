b961a4ec33f3b1e2db82e623032a7ccb8de76e0c
ddai <daichao0805@163.com>
2020-03-08 09:58:23 +0800

    example init

---
+++ example/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted(){
+    console.log(this.msg)
+  },
+  data () {
+    return {
+      msg: 'Hello Vue !'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
